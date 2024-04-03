844b6f9e56552833ce547ed929e0b9fde9989ec7
HelcioCarvalho <53917188+HelcioCarvalho@users.noreply.github.com>
2024-04-03 12:00:06 -0300

    Update siteYoutubeDownloader.py

--- siteYoutubeDownloader.py
+++ siteYoutubeDownloader.py
@@ -1,12 +1,18 @@
 import streamlit as st
 from pytube import YouTube
+import os
+import shutil
 
 def start_download(yt_link, save_path):
     try:
         yt_object = YouTube(yt_link)
         st.write("Baixando:", yt_object.title)
         video = yt_object.streams.get_highest_resolution()
-        video.download(output_path=save_path, filename="video")
+        filename = video.default_filename
+        video.download(output_path=save_path)
+        original_path = os.path.join(save_path, filename)
+        new_path = os.path.join(save_path, yt_object.title + '.' + filename.split('.')[-1])
+        shutil.move(original_path, new_path)
         st.success("Download concluído!")
     except Exception as e:
         st.error(f"Erro: {str(e)}")
@@ -14,7 +20,7 @@ def start_download(yt_link, save_path):
 def main():
     st.markdown("<h1 style='color: red;'>YouTube Downloader</h1>", unsafe_allow_html=True)
 
-    yt_link = st.text_input("Insira o link do vídeo do YouTube:"+" (Será baixado a mais alta qualidade)")
+    yt_link = st.text_input("Insira o link do vídeo do YouTube:"+" (Será baixado na mais alta qualidade)")
     save_path = st.text_input("Insira manualmente o caminho onde deseja salvar o vídeo:"+" - Ex: C:\TEMP")
 
     if st.button("Baixar"):
