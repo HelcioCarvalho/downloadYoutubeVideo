3f3bbbcdef0facec636209458c6f0f2c98c55361
HelcioCarvalho <53917188+HelcioCarvalho@users.noreply.github.com>
2024-04-03 12:17:12 -0300

    Update siteYoutubeDownloader.py

--- siteYoutubeDownloader.py
+++ siteYoutubeDownloader.py
@@ -1,34 +1,27 @@
 import streamlit as st
+import pandas as pd
 from pytube import YouTube
-import os
-import shutil
-
-def start_download(yt_link, save_path):
-    try:
-        yt_object = YouTube(yt_link)
-        st.write("Baixando:", yt_object.title)
-        video = yt_object.streams.get_highest_resolution()
-        filename = video.default_filename
-        video.download(output_path=save_path)
-        original_path = os.path.join(save_path, filename)
-        new_path = os.path.join(save_path, yt_object.title + '.' + filename.split('.')[-1])
-        shutil.move(original_path, new_path)
-        st.success(f"Download concluído! Arquivo salvo em: {new_path}")
-    except Exception as e:
-        st.error(f"Erro: {str(e)}")
-
+import base64
+from io import BytesIO
 def main():
-    st.markdown("<h1 style='color: red;'>YouTube Downloader</h1>", unsafe_allow_html=True)
-
-    yt_link = st.text_input("Insira o link do vídeo do YouTube:"+" (Será baixado na mais alta qualidade)")
-    save_path = st.text_input("Insira manualmente o caminho onde deseja salvar o vídeo:"+" - Ex: C:\TEMP")
-
-    if st.button("Baixar"):
-        progress_bar = st.progress(0)
-        start_download(yt_link, save_path)
-        with st.spinner('Baixando...'):
-            for percent_complete in range(100):
-                progress_bar.progress(percent_complete + 1)
-
-if __name__ == "__main__":
-    main()
+	path = st.text_input('Enter URL of any youtube video')
+	option = st.selectbox(
+     'Select type of download',
+     ('audio', 'highest_resolution', 'lowest_resolution'))
+	
+	matches = ['audio', 'highest_resolution', 'lowest_resolution']
+	if st.button("download"): 
+		video_object =  YouTube(path)
+		st.write("Title of Video: " + str(video_object.title))
+		st.write("Number of Views: " + str(video_object.views))
+		if option=='audio':
+			video_object.streams.get_audio_only().download() 		#base64.b64encode("if file is too large").decode()	
+		elif option=='highest_resolution':
+			video_object.streams.get_highest_resolution().download()
+		elif option=='lowest_resolution':
+			video_object.streams.get_lowest_resolution().download()
+	if st.button("view"): 
+		st.video(path) 
+if __name__ == '__main__':
+	main()
+	
